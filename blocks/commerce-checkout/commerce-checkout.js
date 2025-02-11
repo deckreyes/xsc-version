31efbdd994624e77c4fa49a86262c3a8a9ea502d
deck-adobe <deckreyes@adobe.com>
2025-02-11 09:49:17 -0800

    Reverting back

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -15,13 +15,19 @@ import { render as authProvider } from '@dropins/storefront-auth/render.js';
 import * as authApi from '@dropins/storefront-auth/api.js';
 import AuthCombine from '@dropins/storefront-auth/containers/AuthCombine.js';
 
+// Adyen Dropin Modules
+import * as adyenApi from '@dropins/adyen-checkout-extension/api.js';
+import { render as adyenProvider } from '@dropins/adyen-checkout-extension/render.js';
+import AdyenPaymentMethod from '@dropins/adyen-checkout-extension/containers/AdyenPaymentMethod.js';
+
 import { createModal } from '../modal/modal.js';
 
 export default async function decorate(block) {
   let signInModal = null;
 
   // Initialize Dropins
-  initializers.register(checkoutApi.initialize, {});  
+  initializers.register(checkoutApi.initialize, {});
+  initializers.register(adyenApi.initialize, {});
 
   events.on(
     'authenticated',
@@ -68,7 +74,16 @@ export default async function decorate(block) {
             // $content.innerText = 'Custom Check / Money order handler';
             // ctx.appendHTMLElement($content);
           },
-        });      
+        });
+        context.addPaymentMethodHandler('adyen_cc', {
+          render: (ctx, element) => {
+            if (element) {
+              // clear the element first
+              element.innerHTML = '';
+              adyenProvider.render(AdyenPaymentMethod, ctx)(element);
+            }
+          },
+        });
       },
     },
   })(block);
